079c432bbcbe0dca587891188f33b83963182bf8
trannhalinh260203 <nhaphuongkim123@gmail.com>
2024-11-27 08:11:22 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css';
+import {roboto} from '@/app/ui/fonts'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google';
+import { Roboto } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+
+
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const roboto = Roboto({ subsets: ['vietnamese'], weight:"500" });
+export const lusitana = Lusitana({ subsets: ['latin'], weight:"400" });
+
